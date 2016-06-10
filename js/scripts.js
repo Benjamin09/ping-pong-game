ef101064cb24165edf20c188c0247a616e81a790
Benjamin <angelic_motrsports@yahoo.com>
2016-06-10 15:38:04 -0700

    added prompt box for incert of number,re-wrote jscript data for compulations

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,18 @@
-//back end
-var sift=function(number){
-  var list = [];
-  for (var i = 2; i <= number ; i++) {
-    list.push(i);
-  }
-  for (var ping = 2; pong <=5;pinPong = 15 ; prime++ ){
-    for (var i = 0; i <= number; i++) {
-      if((list[i]%prime) === 0 && list[i] !== prime){
-        list.splice(i,1);
-      }
-    }
-  }
-return list;
-}
-
-//front end
-$(function(){
-  $("form#input").submit(function(event){
+$(document).ready(function() {
+  var number = parseInt(prompt("Please enter a number"));
     event.preventDefault();
 
-    var number = parseInt($("#number").val());
-    var text = "number" + "text").val());  
+for(index = 1; index <= number; index += 1) {
+    if (index % 15 === 0) {
+        $("#list").append("<li>" + "ping-pong" + "</li>");
+    } else if (index % 3 === 0) {
+        $("#list").append("<li>" + "ping" + "</li>");
+    } else if (index % 5 === 0) {
+        $("#list").append("<li>" + "pong" + "<li>");
+    } else {
+        $("#list").append("<li>" + index + "</li>");
+    }
+}
 
-     // output to user of action placed
-    $("#output").text(sift(number)text);
 
-
-  });
 });
